303cf2c551603dea23ab1d5644af7e216d05a6d5
manas0018 <manas68773@gmail.com>
2026-06-19 22:17:14 +0530

    Add Swagger docs

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,30 @@ const express = require('express');
 const { registerUser, loginUser } = require('../controllers/auth.controller');
 
 const router = express.Router();
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ */
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
